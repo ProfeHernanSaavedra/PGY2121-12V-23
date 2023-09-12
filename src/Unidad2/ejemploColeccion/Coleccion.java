c5fb77c20f7bc26d87e191d09599cb657f840b24
USRVI-LC4 <USRVI-LC4@VIAWSE02LC0400.duoc.acad>
2023-09-12 20:15:19 -0300

    ejemplo arraylist

---
+++ src/Unidad2/ejemploColeccion/Coleccion.java
@@ -0,0 +1,23 @@
+
+package Unidad2.ejemploColeccion;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Profe Hernan
+ * 
+ */
+public class Coleccion {
+    
+    public static void main(String[] args) {
+        
+        //declarar el ArrayList
+        //List<String> listas = new ArrayList<>();
+        ArrayList<String> listas = new ArrayList<>();
+        
+        
+        
+    }
+    
+}
